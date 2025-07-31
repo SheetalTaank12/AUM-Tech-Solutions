2567768af5a280b9f5a0d63a3187933aeb1fa5e3
SheetalTaank12 <sheetaltaank74@gmail.com>
2025-07-31 20:43:25 +0530

    update files

--- Devolix/app.js
+++ Devolix/app.js
@@ -27,3 +27,29 @@ const hamburger = document.getElementById('hamburger');
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('active');
     });
+
+
+
+
+
+    const toggleButton = document.getElementById('theme-toggle');
+const icon = document.getElementById('theme-icon');
+const logo = document.getElementById('logo');
+
+// Set initial theme
+if (localStorage.getItem('theme') === 'light') {
+  document.body.classList.add('light-mode');
+  icon.classList.replace('fa-sun', 'fa-moon');
+  logo.src = 'images/aumtech-light.jpg';
+} else {
+  logo.src = 'images/aumtech-dark.jpg';
+}
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+  const isLight = document.body.classList.contains('light-mode');
+
+  icon.classList.replace(isLight ? 'fa-sun' : 'fa-moon', isLight ? 'fa-moon' : 'fa-sun');
+  logo.src = isLight ? 'images/aumtech-light.jpg' : 'images/aumtech-dark.jpg';
+  localStorage.setItem('theme', isLight ? 'light' : 'dark');
+});
