dbe7f7a3fa5179fbd01c643137aa761c4e1e52f8
SheetalTaank12 <sheetaltaank74@gmail.com>
2025-08-01 23:44:33 +0530

    implement send button functioning

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const hamburger = document.getElementById('hamburger');
 const icon = document.getElementById('theme-icon');
 const logo = document.getElementById('logo');
 
-// Set initial theme
+// Set initial theme so that theme persists even after refreshing page 
 if (localStorage.getItem('theme') === 'light') {
   document.body.classList.add('light-mode');
   icon.classList.replace('fa-sun', 'fa-moon');
@@ -57,3 +57,19 @@ toggleButton.addEventListener('click', () => {
 
 
 
+//for sending clients mail to aumtech official mail
+
+function sendEmail(e) {
+    e.preventDefault(); // prevent actual form submission
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    const subject = encodeURIComponent("New message from " + name); //make email subject
+    const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`); //make email body
+
+    window.location.href = `mailto:aumtechsolution@gmail.com?subject=${subject}&body=${body}`; //fills values in gmail by default so that the user only have to click send in gmail app
+     //This tells the browser:
+//"Open the user's email app (like Gmail or Outlook) to send an email to yourname@gmail.com, and fill in the subject and body."
+  }
